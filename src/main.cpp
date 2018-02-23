de8d6d6b35b2c14fed9af05c18b6f71f5592c77f
Student <student@ib.polsl.pl>
2018-02-23 14:45:18 +0100

    Piaty commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <LaborkaConfig.h>
 #include <iostream>
+#include <string>
 
 #ifdef USE_TRIGONOMETRY_DEGREE
 	#include "trygonometria.h"
@@ -8,21 +9,25 @@
 #endif
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	double angle = 90;
+	if (argc > 1)
+	{
+		angle = std::stod(argv[1]);
+	}
 	double angle_rad = angle*3.14/180.0;
 
 	#ifdef USE_TRIGONOMETRY_DEGREE
 		std::cout << degreemath::sin(angle) << std::endl;
-		std::cout << degreemath::cos(angle) << std::endl;
+		/*std::cout << degreemath::cos(angle) << std::endl;
 		std::cout << degreemath::tan(angle) << std::endl;
-		std::cout << degreemath::ctg(angle) << std::endl;
+		std::cout << degreemath::ctg(angle) << std::endl;*/
 	#else	
 		std::cout << sin(angle_rad) << std::endl;
-		std::cout << cos(angle_rad) << std::endl;
+		/*std::cout << cos(angle_rad) << std::endl;
 		std::cout << tan(angle_rad) << std::endl;
-		std::cout << 1/tan(angle_rad) << std::endl;
+		std::cout << 1/tan(angle_rad) << std::endl;*/
 	#endif
 	
 	return 0;
