dc294061e2bc369c2e89f3d30d7a16bab9ded72a
Student <student@ib.polsl.pl>
2018-02-23 14:01:31 +0100

    Drugi commit

---
+++ src/Trygonometria/trygonometria.cpp
@@ -0,0 +1,25 @@
+#include "trygonometria.h"
+#include <cmath>
+double degreemath::sin(double degree) {
+double radian = degree*3.14/180.0;
+return std::sin(radian);
+
+}
+
+double degreemath::cos(double degree) {
+double radian = degree*3.14/180.0;
+return std::cos(radian);
+
+}
+
+double degreemath::tan(double degree) {
+double radian = degree*3.14/180.0;
+return std::tan(radian);
+
+}
+
+double degreemath::ctg(double degree) {
+double radian = degree*3.14/180.0;
+return 1/(std::tan(radian));
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+#include "trygonometria.h"
+
 int main()
 {
-return 0;
+	std::cout << degreemath::sin(90) << std::endl;
+	std::cout << degreemath::cos(90) << std::endl;
+	std::cout << degreemath::tan(90) << std::endl;
+	std::cout << degreemath::ctg(90) << std::endl;
+	return 0;
 }
